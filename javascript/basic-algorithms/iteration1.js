352f45719487b2c52d33ce42004f9df9b1af2b0c
sevcoder <ruben@sevcoder.com>
2022-09-27 16:55:14 +0200

    Iteraction#1

--- javascript/basic-algorithms/iteration1.js
+++ javascript/basic-algorithms/iteration1.js
@@ -1,1 +1,10 @@
-hola
+// JS Basic Algorithms
+
+// Iteración #1: Variables
+
+let myFavoriteHero = "Hulk";
+let x = 50;
+let h = 5, y = 10;
+let z = h + y;
+
+console.log(myFavoriteHero, x, h, y, z)
