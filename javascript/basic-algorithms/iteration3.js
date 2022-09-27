2c290456fd0d34280ddb34cffc6c5a5b30117882
sevcoder <ruben@sevcoder.com>
2022-09-27 18:14:36 +0200

    Iteration #3

---
+++ javascript/basic-algorithms/iteration3.js
@@ -0,0 +1,34 @@
+// JS Basic Algorithms
+// Rubén Álvarez
+
+// Iteración #3: Operadores
+
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+
+console.log("10 por 5 =", 10 * 5);
+
+
+/* 1.2 Divide 10 por 2 y muestra el resultado en un console. */
+
+console.log("10 entre 2 =", 10 / 2);
+
+/* 1.3 Muestra mediante un console el resto de dividir 15 por 9. */
+
+console.log("Resto de 15 entre 9 =", 15 % 9);
+
+/* 1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+teniendo dos variables p = 10 y j = 5. */
+
+let p = 10;
+let j = 5;
+let o = 15;
+console.log(`p + j:`, p, "+", j, "=", o);
+
+
+/* 1.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5. */
+
+let i = 50;
+let c = 10;
+let m = 5;
+console.log(`c * m:`, c, "*", m, "=", i);
