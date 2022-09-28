f92d6da3d9e6a8552f714bb863d4552c9f6fcf0b
sevcoder <ruben@sevcoder.com>
2022-09-28 20:41:05 +0200

    feat: Iteration #1

---
+++ javascript/functions/iteration1.js
@@ -0,0 +1,29 @@
+// JS FUNCTIONS
+// Rubén Álvarez
+
+/* Iteración #1: Buscar el máximo
+Completa la función que tomando dos números como argumento devuelva el más alto. */
+
+let number1 = 3;
+let number2 = 4;
+let majorNumber;
+WhatNumberIsMajor (number1, number2);
+
+function WhatNumberIsMajor (number1, number2) {
+      
+      if (number1 > number2) {
+            majorNumber = number1
+//            console.log(number1);
+      } else if (number1 < number2) {
+            majorNumber = number2
+//            console.log(number2);
+      } else if (number1 == number2) {
+            majorNumber = "Son iguales"
+//            console.log("Son iguales");
+      } else {
+            majorNumber = "Los números no son válidos"
+//            console.log("Los números no son válidos");
+      }
+      return majorNumber
+}
+console.log(majorNumber)
