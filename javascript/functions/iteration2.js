a4c28834d52f1a4fd9d4cda8a99e577bdad529a5
vero <vero@iPro-de-vero.home>
2022-10-05 22:05:30 +0200

    feat: Iteration 2 splitted

---
+++ javascript/functions/iteration2.js
@@ -0,0 +1,20 @@
+/* **Iteración #2: Buscar la palabra más larga**
+Completa la función que tomando un array de strings como argumento devuelva el más largo,
+en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+Puedes usar este array para probar tu función: */
+
+
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+
+function encuentraPalabraMasLarga(arrayQueEnvio) {
+
+    let valorMasGrandeHastaElMomento = "";
+    avengers.forEach((registro) => {
+        if (valorMasGrandeHastaElMomento.length < registro.length) {
+            valorMasGrandeHastaElMomento = registro;
+        }
+    });
+    return valorMasGrandeHastaElMomento;
+};
+
+console.log(encuentraPalabraMasLarga(avengers));
--- javascript/functions/iteration3.js
+++ javascript/functions/iteration3.js
@@ -1,41 +1,4 @@
 
-
-
-
-
-
-/* **Iteración #2: Buscar la palabra más larga**
-Completa la función que tomando un array de strings como argumento devuelva el más largo,
-en caso de que dos strings tenga la misma longitud deberá devolver el primero.
-Puedes usar este array para probar tu función: */
-
-
-
-
-
-/* const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
-
-function encuentraPalabraMasLarga(arrayQueEnvio) {
-
-      let valorMasGrandeHastaElMomento = "";
-
-
-      avengers.forEach((registro) => {
-            if (valorMasGrandeHastaElMomento.length < registro.length) {
-                  valorMasGrandeHastaElMomento = registro;
-      
-            }
-      });
-      return valorMasGrandeHastaElMomento;
-};
-
-console.log(encuentraPalabraMasLarga(avengers));
- */
-
-
-
-
-
 /*  Iteración #3: Calcular la suma
 
 Calcular una suma puede ser tan simple com iterar sobre un array y sumar cada uno de los elementos.
