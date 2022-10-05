0c93aad3b4d9613aa4469684b99a7cdc1e4b93f6
vero <vero@iPro-de-vero.home>
2022-10-05 22:10:48 +0200

    feat: iteration 4 splitted

--- javascript/functions/iteration10.js
+++ javascript/functions/iteration10.js
@@ -1,31 +1,4 @@
 
-/* **Iteración #4: Calcular el promedio**
-
-Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función: */
-
-/* const numbers2 = [12, 21, 38, 5, 45, 37, 6];
-let totalSum2 = 0;
-let averageResult = 0;
-
-average(numbers2);
-
-function average(parameter) {
-    
-    averageResult = 0;
-
-        numbers2.forEach(element => {
-            totalSum2 = totalSum2 + element
-        })
-    
-    averageResult = totalSum2 / numbers2.length;
-    return averageResult;
-}
-
-console.log("Resultado:", averageResult) */
-
-
-
-
 /* **Iteración #5: Calcular promedio de strings**
 Crea una función que reciba por parámetro un array y cuando es un valor number lo sume
  y de lo contrario cuente la longitud del string y lo sume. Puedes usar este array para probar tu función:
---
+++ javascript/functions/iteration4.js
@@ -0,0 +1,23 @@
+/* **Iteración #4: Calcular el promedio**
+
+Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función: */
+
+const numbers2 = [12, 21, 38, 5, 45, 37, 6];
+let totalSum2 = 0;
+let averageResult = 0;
+
+average(numbers2);
+
+function average(parameter) {
+    
+    averageResult = 0;
+
+        numbers2.forEach(element => {
+            totalSum2 = totalSum2 + element
+        })
+    
+    averageResult = totalSum2 / numbers2.length;
+    return averageResult;
+}
+
+console.log("Resultado:", averageResult)
