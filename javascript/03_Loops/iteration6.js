12fc4b2e0d356e2128a83ba047b2b59ca2f8a8e1
imac <imac@iPro-de-imac.home>
2022-10-31 05:19:00 +0100

    feat: Loops_iteration6b

--- javascript/03_Loops/iteration6.js
+++ javascript/03_Loops/iteration6.js
@@ -1,22 +1,53 @@
 // JS LOOPS
 // Rubén Álvarez
 
-/* **Iteración #7: For...of avanzado**
-Usa un bucle **for...of** para recorrer todos los juguetes y añade
-los que tengan más de 15 ventas (sellCount) al array popularToys.
-Imprimelo por consola.. Puedes usar este array: */
+/***Iteración #6: Mixed For...of e includes**
+Usa un bucle **for...of** para recorrer todos los juguetes y elimina
+los que incluyan la palabra gato. Recuerda que puedes usar la función
+***.includes()*** para comprobarlo.Puedes usar este array: */
 
-const popularToys = [];
 const toys = [
-	{id: 5, name: 'Buzz MyYear', sellCount: 10}, 
-	{id: 11, name: 'Action Woman', sellCount: 24}, 
-	{id: 23, name: 'Barbie Man', sellCount: 15}, 
-	{id: 40, name: 'El gato con Guantes', sellCount: 8},
-	{id: 40, name: 'El gato felix', sellCount: 35}
-]
+    {id: 5, name: 'Buzz MyYear'}, 
+    {id: 11, name: 'Action Woman'}, 
+    {id: 23, name: 'Barbie Man'}, 
+    {id: 40, name: 'El gato con Guantes'},
+    {id: 40, name: 'El gato felix'}
+    ];
 
-for (const iterator of toys) {
-    (iterator.sellCount > 15) ? popularToys.push(iterator) : 0 ;
+let i = 0;
+hayGatoEncerrado(toys);
+hayGatoEncerrado(toys);
+
+function hayGatoEncerrado(param) {
+i = 0
+   
+    for (const iterator of param) {
+
+            if (iterator.name.includes("gato")) {
+                param.splice(i, 1)
+                esVerdad = true
+            } else {
+                esVerdad = false
+                // console.log("No hay Gato Encerrado");
+            }
+
+        i++
+    }
+
+     
 }
 
-console.log(popularToys);
+/* for (const iterator of toys) {
+
+    if (iterator.name.includes("gato")) {
+        toys.splice(i, 1)
+        // esVerdad = true
+    } else {
+        esVerdad = false
+        console.log("No hay Gato Encerrado");
+    }
+
+i++
+}
+ */
+console.log(toys);
