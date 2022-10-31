0aa59c137be32aed4a3365b82935d6b9bd356f8f
imac <imac@iPro-de-imac.home>
2022-10-31 02:56:19 +0100

    feat: Loops_iteration3

---
+++ javascript/03_Loops/iteration3.js
@@ -0,0 +1,21 @@
+// JS LOOPS
+// Rubén Álvarez
+
+/* **Iteración #3: Probando For...of**
+Usa un bucle forof para recorrer todos los destinos del array.
+Imprime en un ***console.log*** sus valores.
+Puedes usar este array: */
+
+const placesToTravel = 
+[   
+    'Japon',
+    'Venecia',
+    'Murcia',
+    'Santander',
+    'Filipinas',
+    'Madagascar'
+]
+
+for (const iterator of placesToTravel) {
+    console.log(iterator);
+}
