8d8287cc2cfbf32af3c14eea560e1bf8b301d6cc
imac <imac@iPro-de-imac.home>
2022-11-02 05:38:38 +0100

    feat: RB_ iteration5

---
+++ javascript/04_Review-basics/iteration5.js
@@ -0,0 +1,19 @@
+// JS REVIEW BASICS
+// Rubén Álvarez
+
+/* **Iteración #5: Función rollDice**
+
+Crea una función llamada **rollDice()** que reciba como parametro el numero de caras que queramos que tenga el dado que deberá silumar el codigo dentro de la función. Como hemos dicho, que la función use el parametro para simular una tirada de dado y retornar el resultado. Si no se te ocurre como hacer un numero aleatorio no te preocupes! busca información sobre la función de javascript **Math.random();** */
+
+const allowNumbers = [4, 6, 8, 10, 12, 20];
+const number = 4
+
+console.log(randomGen(number));
+
+function randomGen (sides) {
+
+    return Math.floor(Math.random() * sides)            
+
+}
+
+// console.log(randomGen(4));
