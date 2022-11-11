83ad2c2e7e5736e5677277da6763ef63e25e83a9
imac <imac@iPro-de-imac.home>
2022-11-11 05:34:25 +0100

    feat: ASYN_iterations 3

--- javascript/09_Asynchrony/iteration2/app.js
+++ javascript/09_Asynchrony/iteration2/app.js
@@ -1,6 +1,9 @@
 /* 2.1 Dado el siguiente javascript y html. Añade la funcionalidad necesaria usando fetch() para hacer una consulta a la api cuando se haga click en el botón, pasando como parametro de la api, el valor del input.
  */
 
+/* 2.3 En base al ejercicio anterior. Crea dinamicamente un elemento  por cada petición a la api que diga...'El nombre X tiene un Y porciento de ser de Z' etc etc.
+EJ: El nombre Pepe tiene un 22 porciento de ser de ET y un 6 porciento de ser de MZ. */
+
 // declaraciones
 const baseUrl = "https://api.nationalize.io";
 // let input = "francisco";
@@ -29,16 +32,19 @@ const printData = () => {
     const el = data.country;
     const ruta = document.querySelector(`#list`);
     const string = `<li><h2>${input}</h2></li>`;
+
     ruta.innerHTML += string;
+
     for (const i in el) {
-        const string2 = `<li>Country id: ${el[i].country_id}, probability: ${el[i].probability}</li>`;
+        
+        const string2 = `<li id="li${i}">Country id: ${el[i].country_id}, probability: ${el[i].probability}, porcentaje: ${((el[i].probability) * 100).toFixed(1)} %</li>`;
+        
         ruta.innerHTML += string2;
     }
 }
 
 
 // disparo
-
 const getInput = () => {
     let value = document.querySelector(`#userInput`);
     input = value.value
