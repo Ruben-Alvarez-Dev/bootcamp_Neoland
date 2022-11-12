1f36f206d5e268f0f8bc07bf7f3c6705a6a19d06
imac <imac@iPro-de-imac.home>
2022-11-12 13:32:12 +0100

    feat: 12_JM_exercise4a

---
+++ javascript/12_Jedi-master/exercise4a.js
@@ -0,0 +1,44 @@
+/* Dado el siguiente array de objetos. Crea dinamicamente en el html una lista de div que contenga un elemento h4 para el titulo y otro elemento img para la imagen. */
+
+const places = [
+    {
+      title: "Random title",
+      imgUrl:
+        "https://images.unsplash.com/photo-1506466010722-395aa2bef877?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bG9yZCUyMG9mJTIwdGhlJTIwcmluZ3N8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+      title: "Random title",
+      imgUrl:
+        "https://images.unsplash.com/photo-1570610155223-66279ba81b41?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bG9yZCUyMG9mJTIwdGhlJTIwcmluZ3N8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+      title: "Random title",
+      imgUrl:
+        "https://images.unsplash.com/photo-1570888233388-35d777042d9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGxvcmQlMjBvZiUyMHRoZSUyMHJpbmdzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+      title: "Random title",
+      imgUrl:
+        "https://images.unsplash.com/photo-1490440101319-4c8eb3880432?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGxvcmQlMjBvZiUyMHRoZSUyMHJpbmdzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+      title: "Random title",
+      imgUrl:
+        "https://images.unsplash.com/photo-1460453429228-912eec8be349?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fGxvcmQlMjBvZiUyMHRoZSUyMHJpbmdzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    },
+  ];
+
+  document.body.innerHTML = `<ul id="lista">Lista de DIVs</ul>`
+  const lista = document.querySelector(`#lista`);
+  
+for (const i in places) {
+    
+    lista.innerHTML += `
+    <div>
+    <h4>${places[i].title}</h4>
+    <img src="${places[i].imgUrl}" alt="">
+    </div>`
+
+
+}
+
