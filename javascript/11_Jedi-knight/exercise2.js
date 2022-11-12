423c74ff069817f5b979ea5a800b7257b1c0cf25
imac <imac@iPro-de-imac.home>
2022-11-12 01:56:59 +0100

    feat 11JEDI_KNIGHT_exercise2

---
+++ javascript/11_Jedi-knight/exercise2.js
@@ -0,0 +1,50 @@
+/* Dado el siguiente javascript usa forof y forin para hacer la media del volumen de todos los sonidos favoritos que tienen los usuarios. */
+
+const users = [
+    {
+      name: "Alberto",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 50 },
+        rain: { format: "ogg", volume: 60 },
+        firecamp: { format: "mp3", volume: 80 },
+      },
+    },
+    {
+      name: "Antonio",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 30 },
+        shower: { format: "ogg", volume: 55 },
+        train: { format: "mp3", volume: 60 },
+      },
+    },
+    {
+      name: "Pedro",
+      favoritesSounds: {
+        shower: { format: "mp3", volume: 50 },
+        train: { format: "ogg", volume: 60 },
+        firecamp: { format: "mp3", volume: 80 },
+      },
+    },
+    {
+      name: "Cristina",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 67 },
+        wind: { format: "ogg", volume: 35 },
+        firecamp: { format: "mp3", volume: 60 },
+      },
+    },
+  ];
+  
+let sum = 0;
+i = 0;
+
+for (const iterator of users) {
+    const fsounds = iterator.favoritesSounds;
+
+    for (const key2 in fsounds) {
+        sum = sum + fsounds[key2].volume;
+        i++;
+    }
+}
+
+console.log(sum/i);
